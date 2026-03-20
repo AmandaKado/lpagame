023fa0cf82ebda07ad435a63482df06a7c78f10d
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2026-03-20 16:37:52 -0300

    inicio pygame

---
+++ lpagame.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+print("Setup Started")
+window = pygame.display.set_mode(size=(600, 480)) #criando janela do jogo
+pygame.display.set_caption("L.P.A Game")
+print("Setup Finished")
+
+print("Game Started")
+while True:
+  # Check for all events
+  for event in pygame.event.get():
+    # If the event is of type QUIT then exit the game
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      exit()
