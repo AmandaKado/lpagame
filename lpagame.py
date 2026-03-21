e19e8d0a36df5328e0104ac9c915838c8d163679
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2026-03-20 22:57:12 -0300

    arrumando

--- lpagame.py
+++ lpagame.py
@@ -4,7 +4,9 @@ pygame.init()
 
 print("Setup Started")
 window = pygame.display.set_mode(size=(600, 480)) #criando janela do jogo
-pygame.display.set_caption("L.P.A Game")
+pygame.display.set_caption("Garden of Fruits") #definindo o título da janela
+pygame.display.set_icon(pygame.image.load("assets/frutas/cherry.png"))
+
 print("Setup Finished")
 
 print("Game Started")
